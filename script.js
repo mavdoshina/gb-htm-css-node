7fedefd40c17b4fe2a590fd404ed39b9b6fc1b13
mavdoshina <avdoshina.m.e@yandex.ru>
2022-06-05 22:55:29 +0300

    hover  эффекты

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ document.addEventListener('DOMContentLoaded', function(){
         menuClose = document.querySelector('.header-menu-close');
     menuButton.addEventListener('click', function() {
         menuList.classList.toggle('active-menu');
+        menuButton.classList.toggle('onclick-menu');
     });
 
     menuClose.addEventListener('click', function() {
         menuList.classList.remove('active-menu');
+        menuButton.classList.remove('onclick-menu');
     });
 
 
